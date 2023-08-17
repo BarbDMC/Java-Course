d829f0fb7d7a6548fe6ebeb3e1c80bbfe1443275
BarbDMC <barbaradmorantesc@gmail.com>
2023-08-16 23:28:58 -0500

    Section 21 exercises completed

---
+++ Section 21: Java Coding Exercises - Set 7/Coding Exercise 35: Java Coding Exercise - Arithmetic Operations/Exercise 35.java
@@ -0,0 +1,28 @@
+interface Operation {
+    int perform(int x, int y);
+}
+
+// Complete the classes below by implementing the Operation interface and providing your own implementation of the perform method.
+class Add implements Operation {
+    @Override
+    public int perform(int x, int y) {
+        // Write your code here
+        return x + y;
+    }
+}
+
+class Subtract implements Operation {
+    @Override
+    public int perform(int x, int y) {
+        // Write your code here
+        return x - y;
+    }
+}
+
+class Divide implements Operation {
+    @Override
+    public int perform(int x, int y) {
+        // Write your code here
+        return y <= 0 ? -1 : x / y;
+    }
+}
