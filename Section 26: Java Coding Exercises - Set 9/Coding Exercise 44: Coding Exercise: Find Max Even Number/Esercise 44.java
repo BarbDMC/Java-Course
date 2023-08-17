7f875b0b5d36d89f08cc9f3f4d6d3da46fa121eb
BarbDMC <barbaradmorantesc@gmail.com>
2023-08-17 00:38:58 -0500

    Section 26 completed

---
+++ Section 26: Java Coding Exercises - Set 9/Coding Exercise 44: Coding Exercise: Find Max Even Number/Esercise 44.java
@@ -0,0 +1,18 @@
+import java.util.List;
+import java.util.Optional;
+
+public class FunctionalProgrammingMagic {
+
+    public static int findMaxEvenNumber(List<Integer> numbers) {
+        // Write your code here
+        if (numbers == null || numbers.isEmpty()) {
+            return 0;
+        }
+        
+        Optional<Integer> maxEven = numbers.stream()
+        .filter(number -> number % 2 == 0)
+        .max(Integer::compare);
+        
+        return maxEven.orElse(0);
+    }
+}
